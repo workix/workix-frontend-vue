e16d4ac6f4be6b7757f1ae94dbb99247dca1c2fe
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-06-06 17:31:48 -0300

    added have an account

---
+++ src/components/HaveAnAccount.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="jumbotron">
+		<h3>Have an account?</h3>
+		<p>If you don’t have an account you can create one below by entering your email address/username.<br>
+		A password will be automatically emailed to you.</p>
+		<p><a href="#" class="btn btn-primary">Sign In</a></p>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
