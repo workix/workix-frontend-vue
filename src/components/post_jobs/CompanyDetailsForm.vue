c432712fc58b853c47a222e3ce65447e7865b244
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-06-06 17:33:03 -0300

    added form for jobdetails and company details

---
+++ src/components/post_jobs/CompanyDetailsForm.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="form-group" id="company-name-group">
+    <label for="company-name">Company Name</label>
+    <input type="text" class="form-control" id="company-name" placeholder="Enter company name">
+</div>
+<div class="form-group" id="company-tagline-group">
+    <label for="company-tagline">Tagline (Optional)</label>
+    <input type="text" class="form-control" id="company-tagline" placeholder="Brief description">
+</div>
+<div class="form-group" id="company-description-group">
+    <label for="company-description">Description (Optional)</label>
+    <div class="textarea form-control" id="company-description"></div>
+</div>
+<div class="form-group" id="company-video-group">
+    <label for="company-video">Video (Optional)</label>
+    <input type="text" class="form-control" id="company-video" placeholder="Video URL">
+</div>
+<div class="form-group" id="company-website-group">
+    <label for="company-website">Website (Optional)</label>
+    <input type="text" class="form-control" id="company-website" placeholder="http://">
+</div>
+<div class="form-group" id="company-google-group">
+    <label for="company-google">Google+ Username (Optional)</label>
+    <input type="text" class="form-control" id="company-google" placeholder="yourcompany">
+</div>
+<div class="form-group" id="company-facebook-group">
+    <label for="company-facebook">Facebook Username (Optional)</label>
+    <input type="text" class="form-control" id="company-facebook" placeholder="yourcompany">
+</div>
+<div class="form-group" id="company-linkedin-group">
+    <label for="company-linkedin">LinkedIn Username (Optional)</label>
+    <input type="text" class="form-control" id="company-linkedin" placeholder="yourcompany">
+</div>
+<div class="form-group" id="company-twitter-group">
+    <label for="company-twitter">Twitter Username (Optional)</label>
+    <input type="text" class="form-control" id="company-twitter" placeholder="@yourcompany">
+</div>
+<div class="form-group" id="company-logo-group">
+    <label for="company-logo">Logo (Optional)</label>
+    <input type="file" id="company-logo">
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/post_jobs/JobDetailsForm.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="form-group" id="job-email-group">
+        <label for="job-email">Email</label>
+        <input type="email" class="form-control" id="job-email" placeholder="you@yourdomain.com">
+    </div>
+    <div class="form-group" id="job-title-group">
+        <label for="job-title">Title</label>
+        <input type="text" class="form-control" id="job-title" placeholder="e.g. Web Designer">
+    </div>
+    <div class="form-group" id="job-location-group">
+        <label for="job-location">Location (Optional)</label>
+        <input type="text" class="form-control" id="job-location" placeholder="e.g. New York">
+    </div>
+    <div class="form-group" id="job-region-group">
+        <label for="job-region">Region</label>
+        <select  class="form-control" id="job-region">
+            <option>Choose a region</option>
+            <option>New York</option>
+            <option>Los Angeles</option>
+            <option>Chicago</option>
+            <option>Boston</option>
+            <option>San Francisco</option>
+        </select>
+    </div>
+    <div class="form-group" id="job-type-group">
+        <label for="job-type">Job Type</label>
+        <select  class="form-control" id="job-type">
+            <option>Choose a job type</option>
+            <option>Freelance</option>
+            <option>Part Time</option>
+            <option>Full Time</option>
+            <option>Internship</option>
+            <option>Volunteer</option>
+        </select>
+    </div>
+    <div class="form-group" id="job-category-group">
+        <label for="job-category">Job Category</label>
+        <select  class="form-control" id="job-category">
+            <option>Choose a job category</option>
+            <option>Internet Services</option>
+            <option>Banking</option>
+            <option>Financial</option>
+            <option>Marketing</option>
+            <option>Management</option>
+        </select>
+    </div>
+    <div class="form-group" id="job-description-group">
+        <label for="job-description">Description</label>
+        <div class="textarea form-control" id="job-description"></div>
+    </div>
+    <div class="form-group" id="job-url-group">
+        <label for="job-url">Application Email/URL</label>
+        <input type="text" class="form-control" id="job-url" placeholder="Email or Website URL">
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
