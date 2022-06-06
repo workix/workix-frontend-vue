9c2e6caf98a2817ab5ee598b1cb6d805acb276d1
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-06-06 17:32:28 -0300

    create new route to post a job

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 // import HomeView from '../views/HomeView.vue'
 import IndexView from '../views/IndexView.vue'
 import JobsView from '../views/JobsView.vue'
+import PostJobView from '../views/PostJobView.vue'
 
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     path: '/vagas',
     name: 'vagas',
     component: JobsView
+  },  
+  {
+    path: '/cadastrar_vaga',
+    name: 'cadastrar_vagas',
+    component: PostJobView
   },  
   {
     path: '/about',
