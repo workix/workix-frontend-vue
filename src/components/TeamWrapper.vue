976239a7f10b245c61d3b7b695cd3f4188fb7a22
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-06-07 18:01:36 -0300

    fixed base url

--- src/components/TeamWrapper.vue
+++ src/components/TeamWrapper.vue
@@ -9,7 +9,7 @@
 
 							<!-- Team Member 1 -->
 							<div class="team-member text-center img-rounded">
-								<img src="http://localhost:8080/resources/placeholder/140x140.jpg" class="img-responsive img-circle" alt="" />
+								<img :src="`${baseUrl}/resources/placeholder/140x140.jpg`" class="img-responsive img-circle" alt="" />
 								<h5>John Doe</h5>
 								<h6>Managing Director</h6>
 								<p>Aenean non velit non nulla interdum venenatis. Integer in libero sagittis, consequat est quis, commodo odio. Aliquam eu vulputate neque.</p>
@@ -23,7 +23,7 @@
 
 							<!-- Team Member 2 -->
 							<div class="team-member text-center img-rounded">
-								<img src="http://localhost:8080/resources/placeholder/140x140.jpg" class="img-responsive img-circle" alt="" />
+								<img :src="`${baseUrl}/resources/placeholder/140x140.jpg`" class="img-responsive img-circle" alt="" />
 								<h5>Jane Smith</h5>
 								<h6>Sales Director</h6>
 								<p>Aenean non velit non nulla interdum venenatis. Integer in libero sagittis, consequat est quis, commodo odio. Aliquam eu vulputate neque.</p>
@@ -37,7 +37,7 @@
 
 							<!-- Team Member 3 -->
 							<div class="team-member text-center img-rounded">
-								<img src="http://localhost:8080/resources/placeholder/140x140.jpg" class="img-responsive img-circle" alt="" />
+								<img :src="`${baseUrl}/resources/placeholder/140x140.jpg`" class="img-responsive img-circle" alt="" />
 								<h5>Ryan Hampton</h5>
 								<h6>Key Account Manager</h6>
 								<p>Aenean non velit non nulla interdum venenatis. Integer in libero sagittis, consequat est quis, commodo odio. Aliquam eu vulputate neque.</p>
@@ -51,7 +51,7 @@
 
 							<!-- Team Member 4 -->
 							<div class="team-member text-center img-rounded">
-								<img src="http://localhost:8080/resources/placeholder/140x140.jpg" class="img-responsive img-circle" alt="" />
+								<img :src="`${baseUrl}/resources/placeholder/140x140.jpg`" class="img-responsive img-circle" alt="" />
 								<h5>Marilyn Strickland</h5>
 								<h6>Account Manager</h6>
 								<p>Aenean non velit non nulla interdum venenatis. Integer in libero sagittis, consequat est quis, commodo odio. Aliquam eu vulputate neque.</p>
@@ -65,7 +65,7 @@
 
 							<!-- Team Member 5 -->
 							<div class="team-member text-center img-rounded">
-								<img src="http://localhost:8080/resources/placeholder/140x140.jpg" class="img-responsive img-circle" alt="" />
+								<img :src="`${baseUrl}/resources/placeholder/140x140.jpg`" class="img-responsive img-circle" alt="" />
 								<h5>Harold Chandler</h5>
 								<h6>Support Agent</h6>
 								<p>Aenean non velit non nulla interdum venenatis. Integer in libero sagittis, consequat est quis, commodo odio. Aliquam eu vulputate neque.</p>
@@ -79,7 +79,7 @@
 
 							<!-- Team Member 5 -->
 							<div class="team-member text-center img-rounded">
-								<img src="http://localhost:8080/resources/placeholder/140x140.jpg" class="img-responsive img-circle" alt="" />
+								<img :src="`${baseUrl}/resources/placeholder/140x140.jpg`" class="img-responsive img-circle" alt="" />
 								<h5>Mandy Fisher</h5>
 								<h6>Sales Representative</h6>
 								<p>Aenean non velit non nulla interdum venenatis. Integer in libero sagittis, consequat est quis, commodo odio. Aliquam eu vulputate neque.</p>
@@ -101,7 +101,11 @@
 
 <script>
 export default {
-
+	data(){
+		return{
+			baseUrl: window.location.origin
+		}
+	}
 }
 </script>
 
