35fcfbf1d7d19788cf50aca86e2f17b71fa73763
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-06-07 16:57:24 -0300

    installed axios and added to global

--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
+import Axios from 'axios'
 
-createApp(App).use(router).use(store).mount('#app')
+/**
+ * Vue initialization
+ */
+ const app = createApp(App);
+ app.config.globalProperties.$http = Axios; // Allow axios in all componenets this.$http.get
+
+app.use(router).use(store).mount('#app')
