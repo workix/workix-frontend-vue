1d4138d217421406ca3bbc3bc36795673785cef4
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-06-07 09:36:03 -0300

    finished job details

--- src/views/JobDetailsView.vue
+++ src/views/JobDetailsView.vue
@@ -3,6 +3,97 @@
     <PageLoader />
     <NavBar />
     <HeaderBar />
+
+    <!-- ============ TITLE START ============ -->
+		<section id="title">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12 text-center">
+						<h1>Senior Back End Developer</h1>
+						<h4>
+							<span><i class="fa fa-map-marker"></i>Chicago</span>
+							<span><i class="fa fa-clock-o"></i>Full time</span>
+							<span><i class="fa fa-dollar"></i>50,000-75,000</span>
+						</h4>
+					</div>
+				</div>
+			</div>
+		</section>
+    <!-- ============ TITLE END ============ -->
+
+    <!-- ============ CONTENT START ============ -->
+		<section id="jobs">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-8">
+						<article>
+							<h2>Job Details</h2>
+							<p>Maecenas mollis dictum lectus quis scelerisque. Nulla at rutrum ipsum. Praesent augue quam, facilisis vitae felis vel, convallis convallis nisi. Donec maximus accumsan purus vel tempus. Aenean pretium luctus velit id fermentum. Aenean non velit non nulla interdum venenatis. Integer in libero sagittis, consequat est quis, commodo odio. Aliquam eu vulputate neque. Nunc et massa leo. Vestibulum a pretium dolor. Proin et fermentum sapien. Cras malesuada neque ac purus fermentum, a placerat quam malesuada. Quisque sollicitudin tellus a ex eleifend mattis. In vitae ipsum in mauris vestibulum imperdiet.</p>
+							<p>Maecenas mollis dictum lectus quis scelerisque. Nulla at rutrum ipsum. Praesent augue quam, facilisis vitae felis vel, convallis convallis nisi. Donec maximus accumsan purus vel tempus. Aenean pretium luctus velit id fermentum. Aenean non velit non nulla interdum venenatis. Integer in libero sagittis, consequat est quis, commodo odio. Aliquam eu vulputate neque. Nunc et massa leo. Vestibulum a pretium dolor. Proin et fermentum sapien. Cras malesuada neque ac purus fermentum, a placerat quam malesuada. Quisque sollicitudin tellus a ex eleifend mattis. In vitae ipsum in mauris vestibulum imperdiet.</p>
+							<h3>Requirements</h3>
+							<ul>
+								<li>Aliquam rhoncus justo eget tellus scelerisque, at mollis mi aliquam.</li>
+								<li>Quisque pretium convallis pulvinar.</li>
+								<li>Nulla rutrum nisi mi, iaculis commodo nibh lobortis sed.</li>
+								<li>Sed pulvinar, nunc vitae molestie dapibus, lacus dolor dignissim sapien.</li>
+								<li>Pellentesque ipsum ex, imperdiet quis consequat sed, consectetur ut ante.</li>
+								<li>Aliquam libero felis, mollis vitae elementum vel, bibendum eu tortor.</li>
+								<li>Morbi rhoncus luctus interdum.</li>
+							</ul>
+							<h3>Benefits</h3>
+							<ul>
+								<li>Aliquam rhoncus justo eget tellus scelerisque, at mollis mi aliquam.</li>
+								<li>Quisque pretium convallis pulvinar.</li>
+								<li>Nulla rutrum nisi mi, iaculis commodo nibh lobortis sed.</li>
+								<li>Sed pulvinar, nunc vitae molestie dapibus, lacus dolor dignissim sapien.</li>
+								<li>Pellentesque ipsum ex, imperdiet quis consequat sed, consectetur ut ante.</li>
+								<li>Aliquam libero felis, mollis vitae elementum vel, bibendum eu tortor.</li>
+								<li>Morbi rhoncus luctus interdum.</li>
+							</ul>
+							<h3>How to apply</h3>
+							<p>Vivamus pulvinar <a href="mailto:hr@netvibes.com">hr@netvibes.com</a> lobortis placerat. Cras non est nibh. In a quam id justo aliquam elementum. In cursus urna ac sem tincidunt aliquet. Vivamus a aliquet purus, luctus tincidunt orci.</p>
+							<p>
+								<a href="#" class="btn btn-primary btn-lg">Apply Here</a>
+								&nbsp;
+								<a href="#" class="btn btn-default btn-lg">Apply via LinkedIn</a>
+							</p>
+						</article>
+					</div>
+					<div class="col-sm-4" id="sidebar">
+						<div class="sidebar-widget" id="share">
+							<h2>Share this job</h2>
+							<ul>
+								<li><a href="https://www.facebook.com/sharer/sharer.php?u=http://www.coffeecreamthemes.com/themes/jobseek/site/job-details.html"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="https://twitter.com/home?status=http://www.coffeecreamthemes.com/themes/jobseek/site/job-details.html"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="https://plus.google.com/share?url=http://www.coffeecreamthemes.com/themes/jobseek/site/job-details.html"><i class="fa fa-google-plus"></i></a></li>
+								<li><a href="https://www.linkedin.com/shareArticle?mini=true&amp;url=http://www.coffeecreamthemes.com/themes/jobseek/site/job-details.html&amp;title=Jobseek%20-%20Job%20Board%20Responsive%20HTML%20Template&amp;summary=&amp;source="><i class="fa fa-linkedin"></i></a></li>
+							</ul>
+						</div>
+						<hr>
+						<div class="sidebar-widget" id="company">
+							<h2>About this company</h2>
+							<p><img src="http://localhost:8080/resources/placeholder/300x109.gif" alt="" class="img-responsive"></p>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tristique euismod lorem, a consequat orci consequat a. Donec ullamcorper tincidunt nunc, ut aliquam est pellentesque porta. In neque erat, malesuada sit amet orci ac, laoreet laoreet mauris.</p>
+							<p><a href="company.html" class="btn btn-primary">Read more</a></p>
+						</div>
+						<hr>
+						<div class="sidebar-widget" id="company-jobs">
+							<h2>More jobs from this company</h2>
+							<ul>
+								<li><a href="#">Senior Web Designer</a></li>
+								<li><a href="#">Junior System Administrator</a></li>
+								<li><a href="#">Key Account Manager</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- ============ CONTENT END ============ -->
+      <ContactsWrapper />      
+      <FooterWrapper />
+      <LoginPopup />
+      <RegisterPopup />
   </div>
 </template>
 
@@ -10,11 +101,19 @@
 import PageLoader from '@/components/PageLoader.vue'
 import NavBar from '@/components/NavBar.vue'
 import HeaderBar from '@/components/HeaderBar.vue'
+import ContactsWrapper from "@/components/ContactsWrapper.vue"
+import FooterWrapper from "@/components/FooterWrapper.vue"
+import LoginPopup from "@/components/LoginPopup.vue"
+import RegisterPopup from "@/components/RegisterPopup.vue"
 export default {
     components: {
         PageLoader,
         NavBar,
-        HeaderBar
+        HeaderBar,
+        ContactsWrapper,
+        FooterWrapper,
+        LoginPopup,
+        RegisterPopup
     }
 }
 </script>
