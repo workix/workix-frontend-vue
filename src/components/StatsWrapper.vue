939d84b5b2384ce45b7653d878f993ea8010e39a
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-06-08 13:37:25 -0300

    get stats from api

--- src/components/StatsWrapper.vue
+++ src/components/StatsWrapper.vue
@@ -5,36 +5,36 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-sm-12">
-						<h1>Jobseek Stats</h1>
-						<h4>How many people we’ve helped</h4>
+						<h1>Estatísticas Workix</h1>
+						<h4>Quantas Pessoas Estamos Ajudando</h4>
 					</div>
 				</div>
-				<div class="row" id="counter">
+				<div class="row" id="counter" v-if="this.members && this.jobs && this.resumes && this.companies">
 					
 					<div class="counter">
-						<div class="number">4,329</div>
-						<div class="description">Members</div>
+						<div class="number">{{this.members}}</div>
+						<div class="description">Candidatos</div>
 					</div>
 				
 					<div class="counter">
-						<div class="number">894</div>
-						<div class="description">Jobs</div>
+						<div class="number">{{this.jobs}}</div>
+						<div class="description">Vagas</div>
 					</div>
 				
 					<div class="counter">
-						<div class="number">1,482</div>
-						<div class="description">Resumes</div>
+						<div class="number">{{this.resumes}}</div>
+						<div class="description">Currículos</div>
 					</div>
 				
 					<div class="counter">
-						<div class="number">83</div>
-						<div class="description">Companies</div>
+						<div class="number">{{this.companies}}</div>
+						<div class="description">Empresas</div>
 					</div>
 
 				</div>
 				<div class="row">
 					<div class="col-sm-12">
-						<p><a class="link-register btn btn-primary">Join Us</a></p>
+						<p><a class="link-register btn btn-primary">Junte-se a nós</a></p>
 					</div>
 				</div>
 			</div>
@@ -44,7 +44,27 @@
 
 <script>
 export default {
-
+	data() {
+		return {
+			members: null,
+			jobs: null,
+			resumes: null,
+			companies: null
+		}
+	},
+	methods: {
+		async getStatistics(){
+			const {data} = await this.$http.get("http://localhost:8080/workix/services/v1/statistics")
+			
+			this.members = data.members
+			this.jobs = data.jobs
+			this.resumes = data.resumes
+			this.companies = data.companies			
+		}
+	},
+	async created(){
+		await this.getStatistics()
+	}
 }
 </script>
 
