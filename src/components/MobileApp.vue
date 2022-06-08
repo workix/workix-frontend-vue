34ace04213d057a6173eaf7aa6cac223eadb12e5
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-06-08 19:24:22 -0300

    translated mobile app view

--- src/components/MobileApp.vue
+++ src/components/MobileApp.vue
@@ -8,11 +8,11 @@
 					</div>
 					<div class="col-sm-offset-1 col-sm-6">
 						<p>&nbsp;</p>
-						<h2>Get Jobseek app</h2>
-						<p>Curabitur et lorem a massa tempus aliquam. Aenean aliquam volutpat gravida. Pellentesque in neque nec tortor sagittis tempor quis in lectus. Vestibulum vehicula aliquet elit ut porta. Sed ipsum felis, interdum blandit purus sed, volutpat ultricies sem. Maecenas feugiat, lectus vitae luctus feugiat, nulla nisl dignissim velit, nec malesuada ligula orci et metus. In vulputate laoreet luctus.</p>
-						<p>
-							<a href="#" class="btn btn-default"><i class="fa fa-apple"></i> IOS</a>
-							<a href="#" class="btn btn-default"><i class="fa fa-android"></i> Android</a>
+						<h2>EM BREVE VOCÊ PODERÁ BAIXAR O WORKIX MOBILE</h2>
+						<p>Em Desenvolvimento... Imagine as mesmas vagas, comunicados e alertas...
+							Uma experiência mais confortável em seu aparelho celular. E o melhor Tudo de Graça!</p>
+						<p>							
+							<a href="https://play.google.com/store/apps/details?id=br.com.frmichetti.openjobs.android" class="btn btn-default"><i class="fa fa-android"></i> Android</a>
 						</p>
 						<p>&nbsp;</p>
 					</div>
