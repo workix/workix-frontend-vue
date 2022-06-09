6028f8823161fbb6c21364891a3f6ff50a485d8c
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-06-09 10:34:23 -0300

    translated aboutview and get team from api in teamwrapper

--- src/components/TeamWrapper.vue
+++ src/components/TeamWrapper.vue
@@ -4,9 +4,26 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-sm-12">
-						<h2>Our Team</h2>
+						<h2>Nossa Equipe</h2>
 						<div class="owl-carousel">
 
+							<template v-if="!debug">
+							<!-- Team Member-->
+							<div class="team-member text-center img-rounded" v-for="(m,i) in members" :key="i">
+								<img :src="m.picture" class="img-responsive img-circle" alt="" />
+								<h5>{{m.name}}</h5>
+								<h6>{{m.occupation}}</h6>
+								<p>{{m.shortText}}</p>
+								<p>
+									<template v-for="(mm,idx) in m.medias" :key="idx">
+										<a :href="mm.url"><i :class="getClass(mm.media)"></i></a>&nbsp;&nbsp;&nbsp;
+									</template>	
+									
+								</p>
+							</div>
+							</template>
+
+							<template v-if="debug">
 							<!-- Team Member 1 -->
 							<div class="team-member text-center img-rounded">
 								<img :src="`${baseUrl}/resources/placeholder/140x140.jpg`" class="img-responsive img-circle" alt="" />
@@ -17,7 +34,7 @@
 									<a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>
 									<a href="#"><i class="fa fa-twitter-square fa-2x"></i></a>
 									<a href="#"><i class="fa fa-linkedin-square fa-2x"></i></a>
-									<a href="#"><i class="fa fa-instagram fa-2x"></i></a>
+									<a href="#"><i class="fa fa-instagram fa-2x"></i></a>															
 								</p>
 							</div>
 
@@ -83,13 +100,16 @@
 								<h5>Mandy Fisher</h5>
 								<h6>Sales Representative</h6>
 								<p>Aenean non velit non nulla interdum venenatis. Integer in libero sagittis, consequat est quis, commodo odio. Aliquam eu vulputate neque.</p>
-								<p>
+								<p>									
 									<a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>
 									<a href="#"><i class="fa fa-twitter-square fa-2x"></i></a>
 									<a href="#"><i class="fa fa-linkedin-square fa-2x"></i></a>
 									<a href="#"><i class="fa fa-instagram fa-2x"></i></a>
 								</p>
 							</div>
+							</template>
+							
+							
 
 						</div>
 					</div>
@@ -103,8 +123,35 @@
 export default {
 	data(){
 		return{
-			baseUrl: window.location.origin
+			baseUrl: window.location.origin,
+			debug: true,
+			members: []
 		}
+	},
+	methods:{
+		getMembers(){
+			return this.$http.get("http://localhost:8080/workix/services/v1/members")
+		},
+		getClass(media){
+			switch (media) {
+				case "Facebook":					
+					return "fa fa-facebook-square fa-2x"
+				case "Twitter":
+					return "fa fa-twitter-square fa-2x"		
+				case "Linkedin":
+					return "fa fa-linkedin-square fa-2x"
+				case "instagram":
+					return "fa fa-instagram fa-2x"	
+				case "GitHub":
+					return "fa fa-github fa-2x"
+				default:
+					break;
+			}
+		}
+	},
+	async created(){
+		const {data} = await this.getMembers()
+		this.members = data
 	}
 }
 </script>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -9,8 +9,8 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-sm-12 text-center">
-						<h1>About Us</h1>
-						<h4>Short story of our company</h4>
+            <h1>Sobre nós</h1>
+            <h4>Breve descrição do Nosso Projeto</h4>
 					</div>
 				</div>
 			</div>
@@ -18,14 +18,148 @@
   <!-- ============ TITLE END ============ -->
 
   <!-- ============ STORY START ============ -->
-		<section id="story">
+	<section id="story">
 			<div class="container">
 				<div class="row">
 					<div class="col-sm-6">
-						<p>Maecenas mollis dictum lectus quis scelerisque. Nulla at rutrum ipsum. Praesent augue quam, facilisis vitae felis vel, convallis convallis nisi. Donec maximus accumsan purus vel tempus. Aenean pretium luctus velit id fermentum. Aenean non velit non nulla interdum venenatis. Integer in libero sagittis, consequat est quis, commodo odio. Aliquam eu vulputate neque. Nunc et massa leo. Vestibulum a pretium dolor. Proin et fermentum sapien. Cras malesuada neque ac purus fermentum, a placerat quam malesuada. Quisque sollicitudin tellus a ex eleifend mattis. In vitae ipsum in mauris vestibulum imperdiet.</p>
+						<h2>Afinal o que é Workix ?</h2>
+
+						<p>Workix é uma ferramenta de busca ; Um projeto ; Um site ,
+							Um backend , uma ferramenta que visa facilitar o processo
+							engessado e cansativo de Seleção de Candidatos e publicação de
+							Empregos.</p>
+						
+						<p>O objetivo da Workix é harmonizar os processos de
+							Recrutamento e reduzir as más práticas.</p>
+
+						<h2>Qual empresa administra a Workix?</h2>
+
+						<p>Workix não é empresa, não há pessoas por trás da
+							ferramenta. Somente a inteligência do algoritmo. Pessoas existem
+							nas pontas ... que alimentam o sistema e usufruem das
+							informações, chegando ao resultado final que é a contratação.</p>
+
+						<h2>Diferenciais da Nossa ferramenta.</h2>
+
+						<b>Fornecer FeedBack</b>
+						
+						<p>Quem nunca participou de um processo e nem ao menos recebeu
+							uma resposta do que aconteceu no final?</p>
+
+						<p>Workix mantém a relação de confiança entre as duas pontas,
+							o processo é transparente e comunicatível. Toda Iteração é
+							passivel de alertas...</p>
+
+						<b>Ambiente Acessível de Qualquer dispositivo</b>
+						
+						<p>No Tablet, No Celular, No Notebook, No Pc, No Vídeo Game...
+							Não interessa o ambiente. Workix oferece Site Responsivo e
+							Aplicativo Mobile. As Mesmas Informações em Todos os lugares.</p>
+
+						<h3>Cadastre seu Currículo e visualize aonde precisar...</h3>
+
+
+						<b>Foco em Escalabilidade e Crescimento</b>
+						
+						<p>Workix visa o Crescimento "Orgânico". Novas Ferramentas
+							poderão ser implementadas para aumentar a experiência e
+							automatizar os processos.</p>
+						
+						<em>**Validadores e Verificadores podem ser usados para
+							garantir a Veracidade das informaçoes trocadas pelas pessoas.</em> <br />
+
+						<b>Vagas Fantasmas ou Inexistentes no Banco de Dados </b>
+						
+						<p>Workix possue algoritmos de auto-limpeza e finaliza os
+							processos realizados com "Sucesso" do Banco de Dados. As vagas
+							possuem Validade. E tudo o que acontece você fica sabendo.</p>
+
+						<b>*Privacidade dos Dados</b>
+						
+						<p>Não armazenamos senhas ou nomes de usuários. Apenas os
+							Dados Minimos para inicializar o processo de contratação é
+							armazenado.</p>
+						
+						<p>Você também poderá ativar ou desativar o currículo conforme
+							necessidade.E poderá solicitar a remoção de informaçao caso
+							desejar.</p>
+						
+						<em>**CPF para pessoas Fisicas e CNPJ para pessoas Juridicas
+							,para manter integridade do Cadastro 1 x 1.</em>
+
+
 					</div>
+
 					<div class="col-sm-6">
-						<p>Maecenas mollis dictum lectus quis scelerisque. Nulla at rutrum ipsum. Praesent augue quam, facilisis vitae felis vel, convallis convallis nisi. Donec maximus accumsan purus vel tempus. Aenean pretium luctus velit id fermentum. Aenean non velit non nulla interdum venenatis. Integer in libero sagittis, consequat est quis, commodo odio. Aliquam eu vulputate neque. Nunc et massa leo. Vestibulum a pretium dolor. Proin et fermentum sapien. Cras malesuada neque ac purus fermentum, a placerat quam malesuada. Quisque sollicitudin tellus a ex eleifend mattis. In vitae ipsum in mauris vestibulum imperdiet.</p>
+
+						<h2>Mais uma ferramenta de busca que vende anúncios VIP e
+							Vagas Destaque?</h2>
+						
+						<p>Mais um diferencial da Workix. Nós não vendemos vagas ou
+							acesso a conteúdos de veracidade duvidosa. Nós fornecemos a
+							ferramenta para as pessoas de interesse comum se encontrarem.
+							Empregador x Empregado.</p>
+
+						<h2>Garantias</h2>
+						
+						<p>
+							Nós não fornecemos garantias no processo de recrutamento.<br />
+							Fornecemos a ferramenta de busca para encontrar vagas e damos o
+							feedback. O processo seletivo ou qualquer má prática de seus
+							usuários é de responsabilidade de seus autores.
+						</p>
+
+						<h2>Fase Atual do Projeto</h2>
+						
+						<p>Projeto encontra-se em desenvolvimento e testes... </p>
+							
+							<p>85% de Tradução dos Templates</p>
+							
+							<div class="progress">
+							
+							<div class="progress-bar progress-bar-danger progress-bar-striped active"
+								role="progressbar" aria-valuenow="85" aria-valuemin="0"
+								aria-valuemax="100" style="width: 85%">85%</div>
+							</div>
+						
+							<p>75% do Backend Web Pronto</p>
+							
+							<div class="progress">
+							
+							<div class="progress-bar progress-bar-success progress-bar-striped active"
+								role="progressbar" aria-valuenow="75" aria-valuemin="0"
+								aria-valuemax="100" style="width: 75%">75%</div>
+							</div>
+							
+							<p>60% do CMS Front End Web Pronto</p>
+							
+							<div class="progress">
+							
+							<div class="progress-bar progress-bar-warning progress-bar-striped active"
+								role="progressbar" aria-valuenow="60" aria-valuemin="0"
+								aria-valuemax="100" style="width: 60%">60%</div>
+							</div>
+							
+							<p>20% da Fase de Desenvolvimento Mobile...</p>
+						
+							<div class="progress">
+							
+							<div class="progress-bar progress-bar-info progress-bar-striped active"
+								role="progressbar" aria-valuenow="20" aria-valuemin="0"
+								aria-valuemax="100" style="width: 20%">20%</div>
+							</div>						
+
+						<p>
+							<em>As mesmas Vagas aqui estarão disponíveis em qualquer
+								Lugar.</em>
+						</p>
+
+						<h3>Quem criou o Projeto Workix?</h3>
+						
+						<p>
+							<a href="http://www.codecode.com.br">Felipe Rodrigues Michetti</a>. Desenvolvedor Web e Mobile.
+						</p>
+
 					</div>
 				</div>
 			</div>
