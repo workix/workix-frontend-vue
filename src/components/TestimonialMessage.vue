6cd1c98e7e17b611dac093d805d9d8237529e35e
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-06-09 14:43:37 -0300

    create testimonial message

---
+++ src/components/TestimonialMessage.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>    
+        <div class="col-sm-3 col-md-2">
+            <img :src="testimonial.picture" class="img-circle img-responsive" alt="testimonial" />
+        </div>
+        <div class="col-sm-9 col-md-10">
+            <blockquote>
+                <p>{{testimonial.text}}</p>
+                <footer>
+                    {{testimonial.author.name}}
+                    <cite :title="testimonial.signature">{{testimonial.signature}}</cite>
+                </footer>
+            </blockquote>
+        </div>    
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        testimonial: Object
+    }
+}
+</script>
+
+<style>
+
+</style>
