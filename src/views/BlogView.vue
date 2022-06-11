0c3b5c2917c40fbf456210942849dd95013009b1
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-06-11 09:00:14 -0300

    get data from api

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -10,7 +10,7 @@
 				<div class="row">
 					<div class="col-sm-12 text-center">
 						<h1>Blog</h1>
-						<h4>Latest News</h4>
+						<h4>Últimas notícias</h4>
 					</div>
 				</div>
 			</div>
@@ -24,92 +24,36 @@
 				<div class="row">
 
 					<!-- POSTS START -->
-
 					<div class="col-sm-8">
 						<article>
-
+							<template v-for="(p,i) in posts" :key="i">
 							<div class="post row">
 								<div class="col-sm-2">
-									<img :src="`${baseUrl}/resources/placeholder/140x140.jpg`" alt="" class="img-responsive img-circle" />
+									<img :src="p.author.picture" alt="" class="img-responsive img-circle" />
 								</div>
 								<div class="col-sm-10">
-									<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h2>
+									<h2>{{p.title}}</h2>
 									<div class="meta">
-										<span><i class="fa fa-user"></i>John Doe</span>
-										<span><i class="fa fa-calendar"></i>28/09/2015</span>
-										<span><i class="fa fa-comment"></i>24</span>
+										<span><i class="fa fa-user"></i>{{p.author.name}}</span>
+										<span><i class="fa fa-calendar"></i>{{p.date.toLocaleString("pt-BR")}}</span>
+										<span><i class="fa fa-comment"></i>{{p.comments.length}}</span>
 									</div>
-									<p><img :src="`${baseUrl}/resources/placeholder/800x530.jpg`" alt="" class="img-responsive" /></p>
-									<p>Proin eu erat tincidunt, scelerisque ipsum non, fringilla dolor. Quisque interdum congue tellus. Aliquam vel odio sit amet ex laoreet finibus. Etiam commodo purus tortor, sed interdum justo faucibus vitae. Praesent ac elit a felis luctus facilisis. Aenean et magna elit. Morbi eget nulla volutpat, egestas ex vel, blandit enim.</p>
-									<a href="post.html" class="btn btn-primary">Read more &nbsp; <i class="fa fa-arrow-right"></i></a>
+									<p><img :src="p.pictures[0]" alt="" class="img-responsive" /></p>
+									<p>{{p.resume}}</p>
+									<a :href="`/postagem?id=${p.id}`" class="btn btn-primary">Leia mais &nbsp; <i class="fa fa-arrow-right"></i></a>
 								</div>
 							</div>
 
 							<hr>
-
-							<div class="post row">
-								<div class="col-sm-2">
-									<img :src="`${baseUrl}/resources/placeholder/140x140.jpg`" alt="" class="img-responsive img-circle" />
-								</div>
-								<div class="col-sm-10">
-									<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h2>
-									<div class="meta">
-										<span><i class="fa fa-user"></i>John Doe</span>
-										<span><i class="fa fa-calendar"></i>24/09/2015</span>
-										<span><i class="fa fa-comment"></i>18</span>
-									</div>
-									<p><img :src="`${baseUrl}/resources/placeholder/800x530.jpg`" alt="" class="img-responsive" /></p>
-									<p>Proin eu erat tincidunt, scelerisque ipsum non, fringilla dolor. Quisque interdum congue tellus. Aliquam vel odio sit amet ex laoreet finibus. Etiam commodo purus tortor, sed interdum justo faucibus vitae. Praesent ac elit a felis luctus facilisis. Aenean et magna elit. Morbi eget nulla volutpat, egestas ex vel, blandit enim.</p>
-									<a href="post.html" class="btn btn-primary">Read more &nbsp; <i class="fa fa-arrow-right"></i></a>
-								</div>
-							</div>
-
-							<hr>
-
-							<div class="post row">
-								<div class="col-sm-2">
-									<img :src="`${baseUrl}/resources/placeholder/140x140.jpg`" alt="" class="img-responsive img-circle" />
-								</div>
-								<div class="col-sm-10">
-									<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h2>
-									<div class="meta">
-										<span><i class="fa fa-user"></i>John Doe</span>
-										<span><i class="fa fa-calendar"></i>21/09/2015</span>
-										<span><i class="fa fa-comment"></i>8</span>
-									</div>
-									<p><img :src="`${baseUrl}/resources/placeholder/800x530.jpg`" alt="" class="img-responsive" /></p>
-									<p>Proin eu erat tincidunt, scelerisque ipsum non, fringilla dolor. Quisque interdum congue tellus. Aliquam vel odio sit amet ex laoreet finibus. Etiam commodo purus tortor, sed interdum justo faucibus vitae. Praesent ac elit a felis luctus facilisis. Aenean et magna elit. Morbi eget nulla volutpat, egestas ex vel, blandit enim.</p>
-									<a href="post.html" class="btn btn-primary">Read more &nbsp; <i class="fa fa-arrow-right"></i></a>
-								</div>
-							</div>
-
-							<hr>
-
-							<div class="post row">
-								<div class="col-sm-2">
-									<img :src="`${baseUrl}/resources/placeholder/140x140.jpg`" alt="" class="img-responsive img-circle" />
-								</div>
-								<div class="col-sm-10">
-									<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h2>
-									<div class="meta">
-										<span><i class="fa fa-user"></i>John Doe</span>
-										<span><i class="fa fa-calendar"></i>17/09/2015</span>
-										<span><i class="fa fa-comment"></i>9</span>
-									</div>
-									<p><img :src="`${baseUrl}/resources/placeholder/800x530.jpg`" alt="" class="img-responsive" /></p>
-									<p>Proin eu erat tincidunt, scelerisque ipsum non, fringilla dolor. Quisque interdum congue tellus. Aliquam vel odio sit amet ex laoreet finibus. Etiam commodo purus tortor, sed interdum justo faucibus vitae. Praesent ac elit a felis luctus facilisis. Aenean et magna elit. Morbi eget nulla volutpat, egestas ex vel, blandit enim.</p>
-									<a href="post.html" class="btn btn-primary">Read more &nbsp; <i class="fa fa-arrow-right"></i></a>
-								</div>
-							</div>
-
-							<hr>
+							</template>							
 
 						<!-- PAGINATION START -->
-						<ul class="pagination" data-scroll-reveal>
-							<li class="active"><a href="#">1</a></li>
-							<li><a href="#">2</a></li>
-							<li><a href="#">3</a></li>
-							<li><a href="#">Next &nbsp; <i class="fa fa-angle-right"></i></a></li>
+						<ul class="pagination" data-scroll-reveal v-if="paginator">
+							<li v-if="paginator.currentPage > 1"><a :href="`/blog?pagina=${paginator.currentPage-1}&limite=${paginator.limitRows}`">Anterior &nbsp; <i class="fa fa-angle-left"></i></a></li>
+							<li class="active"><a :href="`/blog?pagina=${paginator.currentPage}&limite=${paginator.limitRows}`">{{paginator.currentPage}}</a></li>
+							<!--<li><a href="#">2</a></li>
+							<li><a href="#">3</a></li>-->
+							<li v-if="paginator.currentPage < paginator.totalPages"><a :href="`/blog?pagina=${paginator.currentPage+1}&limite=${paginator.limitRows}`">Próxima &nbsp; <i class="fa fa-angle-right"></i></a></li>
 						</ul>
 						<!-- PAGINATION END -->
 
@@ -128,13 +72,12 @@
 									<i class="fa fa-circle fa-stack-2x"></i>
 									<i class="fa fa-folder fa-stack-1x fa-inverse"></i>
 								</span>
-								Categories
+								Categorias
 							</h5>
 							<ul>
-								<li><a href="#">Companies</a></li>
-								<li><a href="#">Jobs</a></li>
-								<li><a href="#">Resumes</a></li>
-								<li><a href="#">Market</a></li>
+								<template v-for="(c,i) in categories" :key="i">
+									<li><a :href="`/blog?categoria=${c}`">{{c}}</a></li>								
+								</template>
 							</ul>
 						</div>
 
@@ -144,13 +87,12 @@
 									<i class="fa fa-circle fa-stack-2x"></i>
 									<i class="fa fa-archive fa-stack-1x fa-inverse"></i>
 								</span>
-								Archives
+								Arquivos
 							</h5>
 							<ul>
-								<li><a href="#">April 2015</a></li>
-								<li><a href="#">March 2015</a></li>
-								<li><a href="#">February 2015</a></li>
-								<li><a href="#">January 2015</a></li>
+								<template v-for="(tp,i) in timePeriods" :key="i">
+								<li><a :href="`/blog?periodo=${tp.month} ${tp.year}`">{{getMonth(tp.month)}} {{tp.year}}</a></li>								
+								</template>
 							</ul>
 						</div>
 
@@ -160,13 +102,12 @@
 									<i class="fa fa-circle fa-stack-2x"></i>
 									<i class="fa fa-flag fa-stack-1x fa-inverse"></i>
 								</span>
-								Recent Posts
+								Postagems Recentes
 							</h5>
 							<ul>
-								<li><a href="#"><span class="meta">28/04/2015 by Susie Johnson</span>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></li>
-								<li><a href="#"><span class="meta">26/04/2015 by Matt Berry</span>Proin eu erat tincidunt, scelerisque ipsum non, fringilla dolor.</a></li>
-								<li><a href="#"><span class="meta">24/04/2015 by Spencer Conner</span>Quisque interdum congue tellus. Aliquam vel odio sit amet ex laoreet finibus.</a></li>
-								<li><a href="#"><span class="meta">22/04/2015 by Nadine Carlson</span>Etiam commodo purus tortor, sed interdum justo faucibus vitae.</a></li>
+								<template v-for="(rp,i) in recentPosts" :key="i">
+								<li><a :href="`/postagem?id=${rp.id}`"><span class="meta">{{rp.date.toLocaleString("pt-BR")}} por {{rp.author.name}}</span>{{rp.title}}</a></li>
+								</template>
 							</ul>
 						</div>
 
@@ -176,13 +117,12 @@
 									<i class="fa fa-circle fa-stack-2x"></i>
 									<i class="fa fa-flag fa-stack-1x fa-inverse"></i>
 								</span>
-								Recent Comments
+								Comentários Recentes
 							</h5>
 							<ul>
-								<li><a href="#"><span class="meta">Susie Johnson on</span>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></li>
-								<li><a href="#"><span class="meta">Matt Berry on</span>Proin eu erat tincidunt, scelerisque ipsum non, fringilla dolor.</a></li>
-								<li><a href="#"><span class="meta">Spencer Conner on</span>Quisque interdum congue tellus. Aliquam vel odio sit amet ex laoreet finibus.</a></li>
-								<li><a href="#"><span class="meta">Nadine Carlson on</span>Etiam commodo purus tortor, sed interdum justo faucibus vitae.</a></li>
+								<template v-for="(rc,i) in recentComments" :key="i">
+								<li><a href="#"><span class="meta">{{rc.name}} comentou</span>{{rc.text}}</a></li>
+								</template>
 							</ul>
 						</div>
 
@@ -224,13 +164,91 @@ export default {
     },
 	data(){
 		return{
-			baseUrl: window.location.origin
+			baseUrl: window.location.origin,
+			categories: [],
+			posts:[],
+			timePeriods: [],
+			recentPosts: [],
+			recentComments: [],
+			paginator: null
 		}
 	},
-	created(){
+	async created(){
 	let ckeditor = document.createElement('script');  
     ckeditor.setAttribute('src',"js/settings.js");
     document.head.appendChild(ckeditor);
+
+	let resp;
+	resp = await this.getCategories()
+	this.categories = resp.data
+
+	const page = this.$route.query.pagina
+	const limit = this.$route.query.limite
+	resp = await this.getPosts(page,limit)
+	this.posts = resp.data.rows
+	this.paginator = resp.data
+
+	resp = await this.getTimePeriods()
+	this.timePeriods = resp.data
+
+	resp = await this.getRecentPosts(0,4)
+	this.recentPosts = resp.data
+
+	resp = await this.getRecentComments(0,4)
+	this.recentComments = resp.data
+  }, 
+  methods:{
+	getMonth(month){
+		switch (month) {
+			case 1:				
+				return "Janeiro"
+			case 2:				
+				return "Fevereiro"		
+			case 3:				
+				return "Março"
+			case 4:				
+				return "Abril"
+			case 5:				
+				return "Maio"
+			case 6:				
+				return "Junho"
+			case 7:				
+				return "Julho"
+			case 8:				
+				return "Agosto"
+			case 9:				
+				return "Setembro"
+			case 10:				
+				return "Outubro"
+			case 11:				
+				return "Novembro"
+			case 12:				
+				return "Dezembro"
+			default:
+				return "Desconhecido"
+		}
+	},
+	getCategories(){
+		return this.$http.get("http://localhost:8080/workix/services/v1/blogs/categories")
+	},
+	getTimePeriods(){
+		return this.$http.get("http://localhost:8080/workix/services/v1/blogs/time_periods")
+	},
+	getRecentPosts(start, max){
+		return this.$http.get(`http://localhost:8080/workix/services/v1/blogs/recents?start=${start}&max=${max}`)
+	},
+	getRecentComments(start , max){
+		return this.$http.get(`http://localhost:8080/workix/services/v1/comments/recents?start=${start}&max=${max}`)
+	},
+	getPosts(page, limit){
+		if (!page){
+			page = 1
+		}
+		if(!limit){
+			limit = 10
+		}
+		return this.$http.get(`http://localhost:8080/workix/services/v1/blogs/paginated?page=${page}&limit=${limit}`)
+	}
   }
 }
 </script>
