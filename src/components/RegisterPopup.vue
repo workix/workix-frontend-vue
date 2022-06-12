9f4136832c7d78c519fe4d5b386a6e618993527c
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-06-12 18:38:00 -0300

    added firebase auth

--- src/components/RegisterPopup.vue
+++ src/components/RegisterPopup.vue
@@ -6,7 +6,7 @@
 					<a class="close"><i class="fa fa-remove fa-lg"></i></a>
 					<h2>Register</h2>
 				</div>
-				<form>
+				<form @submit.prevent="registerWithEmailPassword">
 					<ul class="social-login">
 						<li><a class="btn btn-facebook"><i class="fa fa-facebook"></i>Register with Facebook</a></li>
 						<li><a class="btn btn-google"><i class="fa fa-google-plus"></i>Register with Google</a></li>
@@ -14,31 +14,31 @@
 					</ul>
 					<hr>
 					<div class="form-group">
-						<label for="register-name">Name</label>
-						<input type="text" class="form-control" id="register-name">
+						<label for="register-name">Nome</label>
+						<input type="text" v-model="name" class="form-control" id="register-name" required>
 					</div>
-					<div class="form-group">
+					<!--<div class="form-group">
 						<label for="register-surname">Surname</label>
 						<input type="text" class="form-control" id="register-surname">
-					</div>
+					</div>-->
 					<div class="form-group">
 						<label for="register-email">Email</label>
-						<input type="email" class="form-control" id="register-email">
+						<input type="email" v-model="email" class="form-control" id="register-email" required>
 					</div>
 					<hr>
-					<div class="form-group">
+					<!--<div class="form-group">
 						<label for="register-username">Username</label>
 						<input type="text" class="form-control" id="register-username">
+					</div>-->
+					<div class="form-group">
+						<label for="register-password1">Senha</label>
+						<input type="password" v-model="password" class="form-control" id="register-password1" required>
 					</div>
 					<div class="form-group">
-						<label for="register-password1">Password</label>
-						<input type="password" class="form-control" id="register-password1">
+						<label for="register-password2">Confirme a senha</label>
+						<input type="password" v-model="repeatPassword" class="form-control" id="register-password2" required>
 					</div>
-					<div class="form-group">
-						<label for="register-password2">Repeat Password</label>
-						<input type="password" class="form-control" id="register-password2">
-					</div>
-					<button type="submit" class="btn btn-primary">Register</button>
+					<button type="submit" class="btn btn-primary" >Registrar</button>
 				</form>
 			</div>
 		</div>
@@ -46,7 +46,67 @@
 </template>
 
 <script>
+import { useToast } from "vue-toastification";
+import {getAuth, createUserWithEmailAndPassword} from 'firebase/auth'
+const $ = require( "jquery" );
 export default {
+	setup(){
+	// Get toast interface
+    const toast = useToast();
+	return {toast}
+	},
+	data(){
+		return {
+			name: "",
+			email: "",
+			password: "",
+			repeatPassword: "",
+			logingData: null
+		}
+	},
+	methods: {
+		disposeModal(){
+			$("#register").fadeOut(300);
+			$("body").removeClass("no-scroll");			
+		},
+		registerWithGoogle(){
+
+		},
+		async registerWithEmailPassword(){
+			if (this.password != this.repeatPassword){
+				return;
+			}
+
+			try {
+			const logingData = await createUserWithEmailAndPassword(getAuth(), this.email, this.password)	
+			this.logingData = logingData;
+
+			this.disposeModal()
+
+			this.toast.success(`Bem vindo ${this.name} sua conta foi criada com sucesso!`, { timeout: 2000 })
+
+			this.name = ""
+			this.email = ""
+			this.password = ""
+			this.repeatPassword = ""
+
+			
+			
+			} catch (error) {
+				console.error(error)				
+				this.disposeModal()
+				this.toast.error("Ocorreu Algum problema ao criar a conta!" , { timeout: 2000 })
+				this.toast.error(error.message , { timeout: 4000 })
+
+
+				this.password = ""
+				this.repeatPassword = ""
+
+			}
+			
+		}
+
+	}
 
 }
 </script>
