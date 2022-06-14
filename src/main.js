02e0c8da28570d7124cc30e43653e7ea09344d6f
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-06-14 20:31:42 -0300

    get firebaseconfig from .env

--- src/main.js
+++ src/main.js
@@ -14,7 +14,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  
+  apiKey: process.env.VUE_APP_FIREBASE_APIKEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASEURL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.VUE_APP_FIREBASE_APPID
 };
 
 // Initialize Firebase
