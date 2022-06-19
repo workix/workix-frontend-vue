d31b0c519c78d1024b3f9f9463c7817efb144e51
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-06-19 11:11:06 -0300

    translated

--- src/components/HaveAnAccount.vue
+++ src/components/HaveAnAccount.vue
@@ -1,15 +1,23 @@
 <template>
-  <div class="jumbotron">
-		<h3>Have an account?</h3>
-		<p>If you don’t have an account you can create one below by entering your email address/username.<br>
-		A password will be automatically emailed to you.</p>
-		<p><a href="#" class="btn btn-primary">Sign In</a></p>
+  <div class="jumbotron" v-if="!isLoggedIn">
+		<h3>Tem uma conta?</h3>
+		<p>Se você não tiver uma conta, pode criar uma abaixo digitando seu endereço de e-mail/nome de usuário.<br>
+			Uma senha será automaticamente enviada por e-mail para você.</p>
+		<p><a href="#" class="btn btn-primary link-login">Entrar</a> <strong>ou</strong> <a href="#" class="btn btn-primary link-register">Registrar</a></p>
+		
 	</div>
 </template>
 
 <script>
+import {useStore} from 'vuex'
+import {computed} from 'vue'
 export default {
+	setup(){
+	const store = useStore()
+	const isLoggedIn = computed(() => store.state.isLoggedIn)	
 
+	return {isLoggedIn}
+	},
 }
 </script>
 
