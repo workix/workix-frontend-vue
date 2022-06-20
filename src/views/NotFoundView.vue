60362f015f9ff7bcdefe055fa3cff4f058b9bb2c
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-06-20 07:22:46 -0300

    fixed 404

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,11 +1,32 @@
 <template>
   <div id="404">
-    <h1>Route Not Found</h1>
+     <!-- ============ TITLE START ============ -->
+		<section id="title">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12 text-center">
+						<h1>Pagina não encontrada</h1>
+						<h4>TALVEZ VOCÊ SE INTERESSE POR ...</h4>
+            <br/>
+            <br/>
+            <br/>
+            <img src="resources/images/error.jpg" />
+					</div>
+				</div>
+			</div>
+		</section>
+    <!-- ============ TITLE END ============ -->
+
+    <BlogWrapper />
   </div>  
 </template>
 
 <script>
+import BlogWrapper from '@/components/BlogWrapper.vue'
 export default {
+  components:{
+    BlogWrapper
+  }
 
 }
 </script>
